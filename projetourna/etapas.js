c4b762eea93ecfcbce10ca6b4b80da2ce182e715
Gabriel Farias <imgabrielsf@gmail.com>
2022-01-05 19:17:11 -0300

    Urna

---
+++ projetourna/etapas.js
@@ -0,0 +1,34 @@
+let etapas=[
+    {
+        titulo:"VEREADOR",
+        numeros: 5,
+        candidatos: [
+        {
+
+                numero:'38111',
+                nome: "blablabla",
+                partido: 'abc',
+                fotos: [
+                {url:'careca.png', legenda: "Vereador", small: false}
+                ]
+
+        },
+        
+
+        
+        ],
+    }
+
+
+
+
+
+
+
+
+
+];
+
+console.log(etapas);
+
+
